80be68d936896d0ba17791175f6e6b0bb78ce413
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-08-19 16:18:54 +1200

    add svg scroll event listener

---
+++ index.js
@@ -0,0 +1,14 @@
+const path = document.getElementById('path')
+const pathLength = path.getTotalLength()
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength
+path.style.strokeDashoffset = pathLength
+
+window.addEventListener('scroll', () => {
+  const scrollPercentage =
+    (document.documentElement.scrollTop + document.body.scrollTop) /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight)
+  const drawLength = pathLength * scrollPercentage
+  path.style.strokeDashoffset = pathLength - drawLength
+})
